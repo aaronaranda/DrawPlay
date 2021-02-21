545baa73df11519839abb69c24b16cc6efafd1d0
lloe <lauren.a.l173@gmail.com>
2021-02-21 13:50:27 -0700

    Moved mouth, added nose

--- src/Cat.java
+++ src/Cat.java
@@ -17,8 +17,14 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 5;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	
+	private static final int NOSE_HEIGHT = 5;
+	private static final int NOSE_WIDTH = HEAD_DIMENSION/12;
+	// nose starts about 40% from left edge of head
+	private static final int NOSE_X = HEAD_DIMENSION/5 * 2 + 2;
+	private static final int NOSE_Y = HEAD_DIMENSION/5 * 2 + 8;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -41,6 +47,12 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		g2.setColor(Color.black);
+		//Draw nose
+		g2.setColor(Color.pink);
+		x = catX + NOSE_X;
+		y = catY + NOSE_Y;
+		g2.fillOval(x, y, NOSE_WIDTH, NOSE_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
